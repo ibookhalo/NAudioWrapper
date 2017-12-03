2f191ab215b45a8aff5703d8a430894eb7be2e4a
Ibrahim Ahmad <ibrahim.ahmad001@stud.fh-dortmund.de>
2017-12-03 23:22:17 +0100

    Init

---
+++ NAudioWrapper/NAudioWrapper/Player.cs
@@ -0,0 +1,51 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAudioWrapper
+{
+    class Player
+    {
+        private WaveFormat waveFormat;
+        private WaveOutEvent waveOutEvent;
+        public EventHandler<StoppedEventArgs> PlaybackStopped;
+        public Player(WaveFormat waveFormat)
+        {
+            this.waveFormat = waveFormat;
+        }
+        public void Play(byte[] buffer)
+        {
+            try
+            {
+                if (waveOutEvent == null)
+                {
+                    waveOutEvent = new WaveOutEvent();
+                    if (PlaybackStopped != null)
+                    {
+                        waveOutEvent.PlaybackStopped += PlaybackStopped;
+                    }
+                }
+                else
+                {
+                    waveOutEvent.Stop();
+                }
+                waveOutEvent.Init(new RawSourceWaveStream(new MemoryStream(buffer), waveFormat));
+                waveOutEvent.Volume = 1;
+                waveOutEvent.Play();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public void Stop()
+        {
+            waveOutEvent.Stop();
+        }
+    }
+}
---
+++ NAudioWrapper/NAudioWrapper/Recorder.cs
@@ -0,0 +1,60 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAudioWrapper
+{
+    class Recorder
+    {
+        private WaveInEvent waveSource = null;
+        public List<byte> Buffer = null;
+
+        public event EventHandler<StoppedEventArgs> RecordingStopped;
+        public Recorder()
+        {
+            Buffer = new List<byte>();
+        }
+        public void StartRecording()
+        {
+            try
+            {
+                if (waveSource == null)
+                {
+                    waveSource = new WaveInEvent();
+                    waveSource.DataAvailable += WaveSource_DataAvailable;
+                    if (RecordingStopped != null)
+                    {
+                        waveSource.RecordingStopped += RecordingStopped;
+                    }
+                }
+                Buffer.Clear();
+                waveSource.StartRecording();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public void StopRecording()
+        {
+            try
+            {
+                waveSource.StopRecording();
+                Buffer.Clear();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        private void WaveSource_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            Buffer.AddRange(e.Buffer);
+        }
+    }
+}
